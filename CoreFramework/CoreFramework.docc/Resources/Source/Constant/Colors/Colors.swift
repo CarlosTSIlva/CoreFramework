5d99aac548dac0968f02afcf21b132aeb128cb46
CarlosTSIlva <carlosteixeira.tc@hotmail.com>
2025-07-10 11:34:57 -0300

    Add initial files for Colors, Metrics, Typography, and OnboardingView

---
+++ CoreFramework/CoreFramework.docc/Resources/Source/Constant/Colors/Colors.swift
@@ -0,0 +1,8 @@
+//
+//  Colors.swift
+//  
+//
+//  Created by Carlos Silva on 09/07/25.
+//
+
+import Foundation
---
+++ CoreFramework/CoreFramework.docc/Resources/Source/Constant/Text/Typography.swift
@@ -0,0 +1,8 @@
+//
+//  Text.swift
+//  
+//
+//  Created by Carlos Silva on 09/07/25.
+//
+
+import Foundation
